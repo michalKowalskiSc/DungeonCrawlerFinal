dc08456632ff3b254e9b2022b50e28c33ba4886c
Michał Kowalski <michal.kowalski.sc@gmail.com>
2021-04-20 18:11:16 +0200

    init

---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Slot : MonoBehaviour , IDropHandler
+{
+ public GameObject item
+ {
+  get {
+        if (transform.childCount > 0)
+        {
+            return transform.GetChild(0).gameObject;
+        }
+        else {
+            return null;
+        }
+  }
+ }
+
+ #region IdropHandler implementation
+ public void OnDrop(PointerEventData eventData)
+ {
+  if (!item) {
+   DragHandler.item.transform.SetParent(transform);
+  }
+ }
+ #endregion
+}
