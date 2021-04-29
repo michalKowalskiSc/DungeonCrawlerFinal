af6e415ff7d910432aab0e7e855e35ac09a1d6b5
Michał Kowalski <michal.kowalski.sc@gmail.com>
2021-04-29 21:20:30 +0200

    Sloty w ekwipunku

--- Assets/Scripts/DragHandler.cs
+++ Assets/Scripts/DragHandler.cs
@@ -28,9 +28,11 @@ public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        string itemTag = item.tag;
+
         item = null;
 
-        if (transform.parent == startParent || transform.parent == transform.root)  // gdy nie przejdzie
+        if (transform.parent == startParent || transform.parent == transform.root || (transform.parent.tag != itemTag && transform.parent.tag != "SlotInv"))  // gdy nie przejdzie
         {
             transform.position = startPosition;
             transform.SetParent(startParent);
